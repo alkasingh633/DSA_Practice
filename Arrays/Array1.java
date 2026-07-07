28644c4d649b7f8fcf403fdb02b891ad8b91bb09
Alka <alkasingh143179@gmail.com>
2026-07-07 15:43:35 +0530

    07-07-2026

---
+++ Arrays/Array1.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] x = {6,7,8};
+		
+		//indexing
+		System.out.println(x[0]);
+		//updating element
+		x[2]= 45;
+		System.out.println(x[2]);
+		int[] arr = new int[4];
+			
+		arr[0] = 10;
+		arr[1] = 60;
+		arr[2] = 50;
+		arr[3] = 40;
+
+		
+		
+	}
+
+}
