9d27bb50f68e9978b9255a4d8b7c99fce0e7c4e9
Alka <alkasingh143179@gmail.com>
2026-07-09 00:47:51 +0530

    09-07-2026

---
+++ Arrays/MaximumElement.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class MaximumElement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {3,87,45,7,45,9,7};
+		
+		 int max =arr[0];
+		 for(int i =0; i<arr.length; i++) {
+			 if(arr[i]>max ) {
+				 max =arr[i];
+			 }
+			 
+		 }
+		 System.out.println("max = "+max);
+
+	}
+
+}
