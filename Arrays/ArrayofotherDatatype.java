b9da6eb0c63d198871b30ebff8ec75d085ce0512
Alka <alkasingh143179@gmail.com>
2026-07-17 21:20:33 +0530

    17-07-2026

---
+++ Arrays/ArrayofotherDatatype.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class ArrayofotherDatatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String[] arr = {"acvb", "dfgh", "rfgg", "frdcdw"};
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+
+}
